5b11387e99d60f1ee7a565421257d9d73a814325
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-02-04 12:50:00 -0700

    persistor deleted

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //added PersistGate for redux-persistor;
-import { PersistGate } from 'redux-persist/integration/react'
+// import { PersistGate } from 'redux-persist/integration/react'
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 // import {persistor} from './redux/store';
 
 
@@ -16,9 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-07-phonebook">
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
         <App />
-       </PersistGate> 
+       {/* </PersistGate>  */}
       </Provider>
 </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+// import {
+//   persistReducer,
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist'
 
 // import { contactsReducer } from "./contactsSlice/contactsSlice";
 
@@ -18,25 +18,26 @@ import { filterSlice } from "./filterSlice/filterSlice";
 
 
 // config for redux-persist;
-const persistConfig = {
-  key: 'root',
-  storage,
-}
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// }
 
-const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer)
+// const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer)
 
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    // contacts: contactsReducer,
+    contacts:contactsSlice.reducer,
     filter: filterSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      // serializableCheck: {
+      //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      // },
     }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
